b4a18b7a4597941496eba03fcbedfd06f5ac2ee3
PrasadTayade2002 <143004853+PrasadTayade2002@users.noreply.github.com>
2023-08-23 20:06:46 +0530

    Update P1.java

--- GITHub/src/Projects/P1.java
+++ GITHub/src/Projects/P1.java
@@ -3,6 +3,7 @@ package Projects;
 public class P1 {
 	public void m1() {
 		System.out.println("hii");
+		System.out.println("hellooooo")
 	}
 public static void main(String[] args) {
 	System.out.println("HIIIII");
